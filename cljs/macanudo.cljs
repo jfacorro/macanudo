30f1c1f6d93c5075082a929521e533be51897b4a
Juan Facorro <juan.facorro@gmail.com>
2013-03-05 03:57:24 -0300

    Improved style and design. Added generic DOM element generator. Modified format-date function.

--- cljs/macanudo.cljs
+++ cljs/macanudo.cljs
@@ -10,15 +10,29 @@
 (defn set-attribute [elem [k v]]
   (.setAttribute elem (name k) v))
 
-(defn create-element [tag attrs]
+(defn process-child [parent child]
+  (cond (map? child)
+          (doseq [attr-val child] (set-attribute parent attr-val))
+        (string? child)
+          (append parent (.createTextNode js/document child))
+        (vector? child)
+          (append parent (create-element child)))
+  parent)
+  
+(defn create-element [[tag & xs]]
   (let [elem (.createElement js/document (name tag))]
-    (reduce set-attribute elem attrs)
+    (doseq [child xs] (process-child elem child))
     elem))
 
+(defn append [parent child]
+  (.appendChild parent child))
+
 (defn add-comic [parent date]
-  (let [src (str comics-url date ".jpg?raw=true")
-        img (create-element :img {:src src})]
-    (.appendChild parent img)))
+  (let [y-m-d (format-date date [:y "-" :m "-" :d])
+        d-m-y (format-date date [:d "/" :m "/" :y])
+        src   (str comics-url y-m-d ".jpg?raw=true")
+        div   [:div [:h4 d-m-y] [:img {:src src}]]]
+    (append parent (create-element div))))
 
 (defn log [msg]
   (.log js/console msg))
@@ -33,11 +47,12 @@
     (str "0" n) 
     (str n)))
 
-(defn format-date [d]
-  (let [y  (.getFullYear d)
-        M  (-> (.getMonth d) inc leading-0)
-        D  (-> (.getDate d) leading-0)]
-    (str y "-" M "-" D)))
+(defn format-date [date format]
+  (let [y  (.getFullYear date)
+        m  (-> (.getMonth date) inc leading-0)
+        d  (-> (.getDate date) leading-0)
+        date {:y y :m m :d d}]
+    (apply str (map #(if (keyword? %) (% date) %) format))))
 
 (defn next-page [])
 
@@ -45,7 +60,7 @@
   (let [image-list (get-by-id :image-list)
         start      (* current-page page-size)
         today      (dec-date (js/Date.) start)
-        dates      (map (comp format-date (partial dec-date today)) (repeat page-size 1))]
+        dates      (map (partial dec-date today) (repeat page-size 1))]
     (doall (map #(add-comic image-list %) dates))))
   
 (defn init []
