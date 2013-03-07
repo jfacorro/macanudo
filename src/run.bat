0d30b4ed13ecbe1b0119ec4e6fe72aa5634429d4
Juan Facorro <juan.facorro@gmail.com>
2013-03-07 10:50:14 -0300

    Added comics.

--- src/run.bat
+++ src/run.bat
@@ -1,5 +1,6 @@
+@ECHO OFF
 SET JAVA_FLAGS=-Dhttp.proxyHost=localhost -Dhttp.proxyPort=5000
 SET CUR_PATH=%~dp0
-SET TARGET_DIR=%CUR_PATH%log\
-java %JAVA_FLAGS% -cp %CUR_PATH%.;%CUR_PATH%..\bin\clojure-1.5.0-RC16.jar clojure.main -m macanudo %TARGET_DIR%
-PAUSE
+SET TARGET_DIR=%CUR_PATH%..\log\
+ECHO Retrieving today's comic.
+java %JAVA_FLAGS% -cp %CUR_PATH%macanudo-0.1.0-SNAPSHOT-standalone.jar clojure.main -m macanudo %TARGET_DIR%
