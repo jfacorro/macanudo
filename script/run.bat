c1df7b4b426652deb83ed095eb459415a5d807cb
Juan Facorro <juan.facorro@gmail.com>
2013-03-19 15:24:51 -0300

    Added messages to script. Deleted old script file.

--- script/run.bat
+++ script/run.bat
@@ -2,12 +2,14 @@
 SET JAVA_FLAGS=-Dhttp.proxyHost=localhost -Dhttp.proxyPort=5000
 SET CUR_PATH=%~dp0
 SET TARGET_DIR=%CUR_PATH%..\log\
-ECHO Retrieving today's comic.
+ECHO Retrieving today's comic...
 java %JAVA_FLAGS% -cp %CUR_PATH%..\target\macanudo-0.1.0-SNAPSHOT-standalone.jar macanudo %TARGET_DIR%
 cd %CUR_PATH%\..
+ECHO Updating repository from origin...
 git pull
+ECHO Adding all files...
 git add %TARGET_DIR%\.
+ECHO Committing changes...
 git commit -m "Added commit."
-git status
+ECHO Pushing changes...
 git push
-PAUSE
--- src/run.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SET JAVA_FLAGS=-Dhttp.proxyHost=localhost -Dhttp.proxyPort=5000
-SET CUR_PATH=%~dp0
-SET TARGET_DIR=%CUR_PATH%..\log\
-ECHO Retrieving today's comic.
-java %JAVA_FLAGS% -cp %CUR_PATH%macanudo-0.1.0-SNAPSHOT-standalone.jar clojure.main -m macanudo %TARGET_DIR%
